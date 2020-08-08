455b39e263c311608de2e862386f5e0d98a4a55e
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-08 18:53:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,22 @@ def key_for_min_value(name_hash)
 name_hash.each do |name, age|
   i = 0 
   lowest = name_hash[?]
-  if name_hash[i] < name_hash[i+1]
-    i = lowest current age 
-  else
-     puts name_hash.key(i+1) 
-     i += 1 
+  if name_hash[0] < name_hash[i+1]
+    name_hash[0] = lowest current age 
+    i += 1 
+  elsif
+    if name_hash[i] < name_hash[i+1]
+
+    # puts name_hash.key(i+1) 
+    # i += 1 
   end 
+
+def key_for_min_value(name_hash)
+  placeholder = lowest so far  
+  name_hash.each {|value|
+  if current value < next value
+    lowest value = current value 
+  elsif next value < current value
+    lowest value to next value 
+  
+end 
