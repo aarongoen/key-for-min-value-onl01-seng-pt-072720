b1455d035fdd64ad5c73075e6a63d0f9d4f98708
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-08 18:41:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,11 @@
   #compare first value to second 
 
 def key_for_min_value(name_hash)
-  i = 0 
-
-name_hash.collect do |name, age|
-
-  if age[i] < age[i+1]
-     age[i] < age[i+=1]
+name_hash.each do |name, age|
+  lowest = name[0]
+  if name[] < lowest current age[i+1]
+    # 
+    lowest = lowest current age 
   else
      puts name_hash.key(i+1) 
      i += 1 
